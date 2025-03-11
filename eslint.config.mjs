df3a829649b25e660790cd01d08b72bb5fc91a88
ziqinggggg <ziqing0914@gmail.com>
2025-03-12 04:29:22 +0800

    Create eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/quotes": "off",
+      "quotes": "off",
+      "no-useless-escape": "off",
+    },
+  },
+];
+
+export default eslintConfig;
